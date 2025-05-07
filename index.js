f5f4788c1af87c1e81e6cd3fefbd68c5aed615b0
Glory Nenman Kitchinme <glorie.kitch@gmail.com>
2025-05-07 23:03:26 +0100

    mobile

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-const dropdownBtn = document.getElementById('dropdownBtn');
-const dropdownMenu = document.getElementById('dropdownMenu');
-
-dropdownBtn.addEventListener('click', () => {
-  dropdownMenu.classList.toggle('show');
+document.querySelectorAll('.dropdown > .dropdow-btn').forEach(link => {
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+        const dropdownMenu = this.nextElementSibling;
+        dropdownMenu.classList.toggle('show');
+    });
 });
 
-document.addEventListener('click', (e) => {
-  if (!dropdownBtn.contains(e.target) && !dropdownMenu.contains(e.target)) {
-    dropdownMenu.classList.remove('show');
-  }
+document.addEventListener('click', function (e) {
+    document.querySelectorAll('.dropdown-menu').forEach(menu => {
+        if (!menu.previousElementSibling.contains(e.target) && !menu.contains(e.target)) {
+            menu.classList.remove('show');
+        }
+    });
 });
 
-function myFunction(event) {
+
+function toggleMobileMenu(event) {
     event.preventDefault();
     const menu= document.getElementById("mobileMenu");
     menu.classList.toggle("active");
